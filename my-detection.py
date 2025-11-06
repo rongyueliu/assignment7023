bb999869660e27eb56906a959cf2caf8b9bb67b1
rongyueliu <1668418688@qq.com>
2025-11-06 21:06:37 +0800

    update

---
+++ my-detection.py
@@ -0,0 +1,20 @@
+
+import jetson.inference
+import jetson.utils
+from jetson_inference import detectNet
+from jetson_utils import videoSource,videoOutput
+
+net=detectNet("ssd-mobilenet-v2",threshold=0.5)
+camera=videoSource("/dev/video0")  #'/dev/video0' for V4L2
+display=videoOutput("display://0")#'my_video.mp4'for file
+
+while display.IsStreaming():#main loop will go here
+       img=camera.Capture()
+
+       if img is None:#capture timeout
+          continue
+      
+       detections=net.Detect(img)
+
+       display.Render(img)
+       display.SetStatus("Object Detection | Network {:.0f}FPS".format(net.GetNetworkFPS()))
